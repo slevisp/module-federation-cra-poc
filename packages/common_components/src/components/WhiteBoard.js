6f4f97dfbfd783436c65ec97bad43c6d5e12910c
slevisp <sunpeng9285@gmail.com>
2022-10-17 22:58:51 +0800

    Fix: change white box background

--- packages/common_components/src/components/WhiteBoard.js
+++ packages/common_components/src/components/WhiteBoard.js
@@ -5,7 +5,7 @@ export default React.forwardRef((props, ref) => {
   return (
     <div
       ref={ref}
-      className='flex justify-start items-center flex-col p-8 h-40 w-60 rounded-3xl text-lg text-primary bg-white border'
+      className='flex justify-start items-center flex-col p-8 h-40 w-60 rounded-3xl text-lg text-primary bg-grey border'
     >
       {data.map((item, index) => (
         <span key={`${item}-${index}`} className='text-lg'>
